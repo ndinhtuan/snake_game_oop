bf9bd682bf78e46d390df43dae849b26fd6a14df
theiron97 <theiron1997@gmail.com>
2015-12-27 10:32:36 +0700

    Update snake_game.h

--- snake_game.h
+++ snake_game.h
@@ -1,22 +1,45 @@
-#include <iostream>
-#include "snake.h"
-#include <conio.h>
+#include <Windows.h>
+#include <vector>
 
-using namespace std;
+#pragma once
 
-int main(){
-	snake ob;
-	ob.veKhung();
+#define HEIGHT 20
+#define WIDTH 40
+#define X0 18
+#define Y0 10
+#define CHAR 219
 
-	while (1){
-		ob.veRan();
-		ob.veTao();
-		
-		ob.dieuKhien();
-		ob.kiemTraAn();
-		ob.kiemTraThua();
-	}
+struct ToaDo{
+	int x0;
+	int y0;
+	int x;
+	int y;
+};
 
-	_getch();
-	return 0;
-}
+enum STYLE{LEFT = 0, RIGHT, UP, DOWN};
+
+class snake
+{
+private:
+	ToaDo head;
+	std::vector<ToaDo>  tail;
+	bool gameOver;
+	int prev;
+	unsigned int score;
+	ToaDo tao;
+	int soDuoi;
+	int speed = 50;
+public:
+	snake();
+	~snake();
+	void veKhung();
+	void veRan();
+	void dieuKhien();
+	void xoaDiem(int x, int y);
+	void kiemTraThua();
+	void veTao();
+	void veDiem();
+	void kiemTraAn();
+	void initPrev(ToaDo& td);
+};
+
